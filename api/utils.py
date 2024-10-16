21ea3bef2e5bb93778342d29f7d980bf58934c9f
uksus14 <akylov7adil92@gmail.com>
2024-10-16 23:04:46 +0100

    bugfix

--- api/utils.py
+++ api/utils.py
@@ -3,7 +3,7 @@ import json
 
 def HttpResponse(*args, **kwargs):
     kwargs["headers"] = kwargs.get("headers", {})|{"Access-Control-Allow-Origin": "https://uksus14.github.io"}
-    return http.HttpResponse()
+    return http.HttpResponse(*args, **kwargs)
 
 def JsonResponse(data: list[dict]|dict):
     return HttpResponse(json.dumps(data), headers={"Content-Type": "application/json"})
