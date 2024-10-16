2d5cb3a3b29ac45ac3ef7ab4972049289963865b
uksus14 <akylov7adil92@gmail.com>
2024-10-16 22:20:43 +0100

    removed slash

--- api/__init__.py
+++
@@ -1,8 +0,0 @@
-from django import http
-class CustomHttpResponse(http.HttpResponse):
-    def __init__(self, *args, **kwargs):
-        kwargs["headers"] = kwargs.get("headers", {})
-        kwargs["headers"]["Access-Control-Allow-Origin"] = "https://uksus14.github.io/"
-        super().__init__(*args, **kwargs)
-
-http.HttpResponse = CustomHttpResponse
--- api/utils.py
+++ api/utils.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 import json
 
 def JsonResponse(data: list[dict]|dict):
-    return HttpResponse(json.dumps(data), headers={"Content-Type": "application/json", "Access-Control-Allow-Origin": "https://uksus14.github.io/"})
+    return HttpResponse(json.dumps(data), headers={"Content-Type": "application/json", "Access-Control-Allow-Origin": "https://uksus14.github.io"})
